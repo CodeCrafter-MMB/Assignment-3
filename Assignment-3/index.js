3503998cd5ce8d709a93e6f0ec3d5b4b55f7f0d0
Muhammad Mubashir <mubashirbehlum606@gmail.com>
2024-08-21 17:25:00 +0500

    Add files via upload

---
+++ Assignment-3/index.js
@@ -0,0 +1,90 @@
+//Q.1
+
+// // Initialize the variable
+// var a = 10;
+
+// // Display the result in the browser
+// document.write("Result:<br>");
+// document.write("The value of a is:<br> " + a);
+// // Perform ++a (pre-increment)
+// document.write("The value of ++a is: " + (++a) + "<br>");
+// document.write("Now the value of a is: " + a + "<br>");
+
+// // Perform a++ (post-increment)
+// document.write("The value of a++ is: " + (a++) + "<br>");
+// document.write("Now the value of a is: " + a + "<br>");
+
+// // Perform --a (pre-decrement)
+// document.write("The value of --a is: " + (--a) + "<br>");
+// document.write("Now the value of a is: " + a + "<br>");
+
+// // Perform a-- (post-decrement)
+// document.write("The value of a-- is: " + (a--) + "<br>");
+// document.write("Now the value of a is: " + a + "<br>");
+
+//Q.2
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write("Result: " + result);
+
+//Q.3
+
+// var myname = prompt("Enter your Name");
+
+// var greet = alert("wellcome to JS")
+
+// var result = ("greet")
+
+//Q.4
+// var input = prompt("Enter a number");
+
+// var num1= [1,2,3,4,5,6,7,8,9,10];
+// var result1 = input*num1[0];
+// document.write(input ,"x" ,num1[0] ,"=", result1 , "<br>")
+
+
+// var num2= [1,2,3,4,5,6,7,8,9,10];
+// var result2 = input*num2[1];
+// document.write(input ,"x" ,num2[1] ,"=", result2 , "<br>")
+
+
+// var num3= [1,2,3,4,5,6,7,8,9,10];
+// var result3 = input*num3[2];
+// document.write(input ,"x" ,num3[2] ,"=", result3 , "<br>")
+
+
+// var num4= [1,2,3,4,5,6,7,8,9,10];
+// var result4 = input*num4[3];
+// document.write(input ,"x" ,num4[3] ,"=", result4 , "<br>")
+
+
+// var num5= [1,2,3,4,5,6,7,8,9,10];
+// var result5 = input*num5[4];
+// document.write(input ,"x" ,num5[4] ,"=", result5 , "<br>")
+
+
+// var num6= [1,2,3,4,5,6,7,8,9,10];
+// var result6 = input*num6[5];
+// document.write(input ,"x" ,num6[5] ,"=", result6 , "<br>")
+
+
+// var num7= [1,2,3,4,5,6,7,8,9,10];
+// var result7 = input*num7[6];
+// document.write(input ,"x" ,num7[6] ,"=", result7 , "<br>")
+
+
+// var num8= [1,2,3,4,5,6,7,8,9,10];
+// var result8 = input*num8[7];
+// document.write(input ,"x" ,num8[7] ,"=", result8 , "<br>")
+
+
+// var num9= [1,2,3,4,5,6,7,8,9,10];
+// var result9 = input*num9[8];
+// document.write(input ,"x" ,num9[8] ,"=", result9 , "<br>")
+
+
+// var num10= [1,2,3,4,5,6,7,8,9,10];
+// var result10 = input*num10[9];
+// document.write(input ,"x" ,num10[9] ,"=", result10 , "<br>")
+
+
